8e6794624f49b4ff0420c09e1c84c84f8da1277d
Matheus S Alves <matheus.souza.alves@gmail.com>
2024-07-29 02:09:56 -0300

    style: responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,9 @@ import ComputerCanvas from './Computer'
 const Hero = () => {
   return (
     <section className={`relative flex h-svh w-full flex-col overflow-hidden lg:flex-row`}>
-      <div className={` ${styles.paddingX} ${styles.paddingY} flex flex-col items-center justify-center gap-5 lg:w-2/4`}>
+      <div
+        className={` ${styles.paddingX} ${styles.paddingY} flex flex-col items-center justify-center gap-5 lg:w-2/4 lg:items-start`}
+      >
         <h1 className={`${styles.heroHeadText} text-white`}>
           Oi, eu sou o <span className="text-primary">Matheus</span>
         </h1>
@@ -14,7 +16,11 @@ const Hero = () => {
         </p>
       </div>
 
-      <img className="absolute top-1/4 lg:top-0 z-[-1] h-1/2 lg:left-1/3 lg:h-screen" src={heroBg} alt="hero background" />
+      <img
+        className="absolute top-1/4 z-[-1] h-1/2 lg:left-1/3 lg:top-0 lg:h-screen"
+        src={heroBg}
+        alt="hero background"
+      />
       <ComputerCanvas />
 
       {/* <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
