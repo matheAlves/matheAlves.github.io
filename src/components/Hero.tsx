8817be4f0fbc0f95f98f4cd5889f3331327b51a6
Matheus S Alves <matheus.souza.alves@gmail.com>
2024-08-05 00:46:20 -0300

    style: improved responsive design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ import linkedin from '../assets/linkedin.png'
 const Hero = () => {
   return (
     <WavyBackground>
-      <section className={`relative flex h-screen flex-col items-center lg:flex-row`}>
+      <section className={`relative flex min-h-screen flex-col items-center lg:flex-row`}>
         <div className={` ${styles.paddingX} ${styles.paddingY} flex flex-col items-center justify-center`}>
           <h1 className={`${styles.heroHeadText}`}>
             Oi, eu sou o <span className="text-primary">Matheus</span>
@@ -16,16 +16,24 @@ const Hero = () => {
             Um desenvolvedor fullstack entusiasta da <span className="text-[#915EFF]">criatividade</span>.
           </p>
 
-          <div className="flex gap-5 mt">
-            <img className={`${styles.heroLink} w-10 h-10 lg:w-16 lg:h-16 mt-5 `} src={github} alt="github logo" onClick={() => window.open('https://github.com/matheAlves', '_blank')}/>
-            <img className={`${styles.heroLink} w-[2.75rem] h-10 lg:w-[4.5rem] lg:h-16 mt-5`} src={linkedin} alt="linkedin logo" onClick={() => window.open('https://www.linkedin.com/in/mathealves/', '_blank')}/>
+          <div className="mt flex gap-5">
+            <img
+              className={`${styles.heroLink} mt-5 h-10 w-10 lg:h-16 lg:w-16`}
+              src={github}
+              alt="github logo"
+              onClick={() => window.open('https://github.com/matheAlves', '_blank')}
+            />
+            <img
+              className={`${styles.heroLink} mt-5 h-10 w-[2.75rem] lg:h-16 lg:w-[4.5rem]`}
+              src={linkedin}
+              alt="linkedin logo"
+              onClick={() => window.open('https://www.linkedin.com/in/mathealves/', '_blank')}
+            />
           </div>
         </div>
-
-        {/* <ComputerCanvas /> */}
         <img className="w-80 lg:w-auto" src={matheus} alt="hero background" />
 
-        <div className="absolute bottom-32 flex w-full items-center justify-center xs:bottom-10">
+        <div className="absolute bottom-10 md:bottom-10 flex w-full items-center justify-center ">
           <a href="#about">
             <div className="flex h-[64px] w-[35px] items-start justify-center rounded-3xl border-4 border-primary p-2">
               <motion.div
--- src/styles.ts
+++ src/styles.ts
@@ -3,8 +3,8 @@ const styles = {
   paddingY: 'sm:py-16 py-6',
   padding: 'sm:px-16 px-6 sm:py-16 py-10',
 
-  heroHeadText: 'font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-tight',
-  heroSubText: 'text-white font-medium lg:text-[40px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]',
+  heroHeadText: 'font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] leading-tight',
+  heroSubText: 'text-white font-medium lg:text-[40px] sm:text-[26px] xs:text-[20px] text-[16px] leading-tight',
 
   heroLink:
     'hover:cursor-pointer transition-transform duration-200 ease-in-out transform hover:scale-110 hover:cursor-pointer hover:cursor-pointer',
