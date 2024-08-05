f50ff49a6115d627b8e7e38d133eab209246d9b2
Matheus S Alves <matheus.souza.alves@gmail.com>
2024-08-05 01:16:43 -0300

    style: responsiveness

--- src/components/WavyBackground.tsx
+++ src/components/WavyBackground.tsx
@@ -97,7 +97,7 @@ export const WavyBackground = ({
   }, [])
 
   return (
-    <div className={cn(`${isMobile && 'h-screen'} flex flex-col items-center justify-center`, containerClassName)}>
+    <div className={cn('flex h-screen flex-col items-center justify-center', containerClassName)}>
       <canvas
         className="absolute inset-0 z-0"
         ref={canvasRef}
