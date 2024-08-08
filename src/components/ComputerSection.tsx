21fa2c2573eab02d8c3877b88f83ad9591c45adc
Matheus S Alves <matheus.souza.alves@gmail.com>
2024-08-08 02:54:26 -0300

    style: change computer section to h svh

--- src/components/ComputerSection.tsx
+++ src/components/ComputerSection.tsx
@@ -2,7 +2,7 @@ import ComputerCanvas from './Computer'
 
 const ComputerSection = () => {
   return (
-    <section className="bg-2000-pattern h-screen bg-cover flex justify-center items-center flex-col">
+    <section className="bg-2000-pattern h-svh bg-cover flex justify-center items-center flex-col">
       <ComputerCanvas />
       <h1 className="text-4xl">em construção</h1>
     </section>
