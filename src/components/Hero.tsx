980281c48ec41805f4b07d53dd607f393d473e71
Matheus S Alves <matheus.souza.alves@gmail.com>
2024-08-08 19:49:44 -0300

    style: fix alignment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
   const textOpacity = useTransform(scrollY, [0, 500], [0.9, 0])
 
   return (
-    <WavyBackground className={`relative flex min-h-screen flex-col items-center md:flex-row ${styles.paddingX}`}>
+    <WavyBackground className={`relative flex min-h-screen flex-col items-center md:flex-row`}>
       <motion.div
         style={{ x: textX, opacity: textOpacity }}
         initial={{ opacity: 0, x: -100 }}
