8adbb4442424a6aab6033e31f6a88374e465312e
Matheus S Alves <matheus.souza.alves@gmail.com>
2024-10-14 04:13:45 -0300

    refactor: update Cyber component scroll effect

--- src/components/Cyber.tsx
+++ src/components/Cyber.tsx
@@ -1,8 +1,8 @@
 import { motion, useScroll, useTransform } from "framer-motion";
 const Cyber = () => {
     const { scrollYProgress } = useScroll();
-    const isMobile = window.matchMedia('(max-width: 500px)').matches;
-    const imageY = useTransform(scrollYProgress, [0, 1], [0, isMobile ? -300  : -600]);
+    // const isMobile = window.matchMedia('(max-width: 500px)').matches;
+    const imageY = useTransform(scrollYProgress, [0, 1], [0, 700]);
 
 
     return (
@@ -13,7 +13,7 @@ const Cyber = () => {
             transition={{ duration: 1 }}
             viewport={{ once: false }}
 
-            className="bg-cyber-goddess md:bg-cover h-full md:h-[1526px] absolute w-full z-0 bg-contain bg-bottom bg-no-repeat"
+            className="bg-cyber-goddess md:bg-cover h-full md:h-[1526px] absolute w-full z-0 bg-contain bg-bottom md:bg-top bg-no-repeat"
         >
 
         </motion.div>
