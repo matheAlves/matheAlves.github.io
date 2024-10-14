228dc90fe87dd9f8d79e7c64fe525d4217beb9be
Matheus S Alves <matheus.souza.alves@gmail.com>
2024-10-14 00:34:30 -0300

    refactor: update Contact component form styles and add form validation
    Refactored the Contact component to update the form styles using the `styles.opacity` class from the `styles.ts` file. Also added form validation to ensure all fields are filled before submitting the form. Displayed success and error toasts accordingly.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,6 +2,7 @@ import { useState, useRef } from 'react';
 import { motion } from 'framer-motion';
 import emailjs from '@emailjs/browser';
 import toast from 'react-hot-toast';
+import { styles } from '@/styles';
 
 const Contact = () => {
 
@@ -23,16 +24,28 @@ const Contact = () => {
 
   const sendEmail = (e: React.FormEvent<EmailForm>) => {
     e.preventDefault();
+    const elements = formRef.current?.elements as EmailFormElements;
+    const { user_name, user_email, message } = elements;
+    if (!user_name.value || !user_email.value || !message.value) {
+      toast.error('por favor, preencha todos os campos', {
+        duration: 5000,
+      });
+      return;
+    }
 
     emailjs
       .sendForm(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, formRef.current ?? '', EMAILJS_PUBLIC_KEY)
       .then(
         () => {
-          toast.success('obrigado, sua mensagem foi recebida com sucesso!');
+          toast.success('obrigado, sua mensagem foi recebida com sucesso! entrarei em contato assim que possível.', {
+            duration: 5000,
+          });
           formRef.current?.reset();
         },
         () => {
-          toast.error('houve um erro ao enviar a mensagem, tente novamente mais tarde.');
+          toast.error('houve um erro ao enviar a mensagem, tente novamente mais tarde.', {
+            duration: 5000,
+          });
         },
       );
   };
@@ -44,23 +57,23 @@ const Contact = () => {
       <h1
         className="text-primary font-monumentBlack text-4xl md:text-5xl lg:text-5xl"
       >contato</h1>
-      <form className='flex flex-[0.2]  flex-col justify-around  text-white' ref={formRef} onSubmit={sendEmail}>
+      <form className='flex flex-[0.2] flex-col justify-around  text-white' ref={formRef} onSubmit={sendEmail}>
         <input
           type="text"
           name="user_name"
-          className='h-10 p-2'
-          placeholder="nome"
+          className={`${styles.opacity} h-10 p-2`}
+        placeholder="nome"
         />
         <input
           type="email"
           name="user_email"
-          className='h-10 p-2'
+          className={`${styles.opacity} h-10 p-2`}
 
           placeholder="email"
         />
         <textarea
           name="message"
-          className='h-32 p-2'
+          className={`${styles.opacity} h-32 p-2`}
 
           placeholder="mensagem"
         />
@@ -70,8 +83,8 @@ const Contact = () => {
       </form>
 
       <footer className="absolute bottom-0 text-center">
-        <p className="text-primary   text-xs">© 2024 · Matheus Alves</p>
-        <p className="text-primary   text-xs">em construção.</p>
+        <p className="text-primary text-xs">© 2024 · Matheus Alves</p>
+        <p className="text-primary text-xs">em construção.</p>
       </footer>
     </motion.div>
 
