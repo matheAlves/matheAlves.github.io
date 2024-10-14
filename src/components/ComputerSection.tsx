b643127df17e446bc674f9c5b0cba5c85f4ff5ee
Matheus S Alves <matheus.souza.alves@gmail.com>
2024-10-14 14:22:08 -0300

    refactor: update Cyber component scroll effect opacity and fix ComputerSection font style

--- src/components/ComputerSection.tsx
+++ src/components/ComputerSection.tsx
@@ -4,7 +4,9 @@ const ComputerSection = () => {
   return (
     <section className="bg-2000-pattern h-svh bg-cover flex justify-center items-center flex-col z-10 relative">
       <ComputerCanvas />
-        <p className="font-monumentLightItalic text-primary text-xs">...but the sun is eclipsed by the moon.</p>
+        <a 
+        href='https://genius.com/823422' target='_blank' rel='noreferrer'
+        className="font-monumentLightItalic text-primary text-[0.6rem]">...but the sun is eclipsed by the moon.</a>
 
     </section>
   )
