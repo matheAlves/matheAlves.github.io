2e802c04e2a47dfd26bd68f720ec4e654547aa39
Matheus S Alves <matheus.souza.alves@gmail.com>
2024-10-14 10:45:09 -0300

    refactor: update Cyber component scroll effect opacity

--- src/components/Cyber.tsx
+++ src/components/Cyber.tsx
@@ -3,15 +3,12 @@ const Cyber = () => {
     const { scrollYProgress } = useScroll();
     // const isMobile = window.matchMedia('(max-width: 500px)').matches;
     const imageY = useTransform(scrollYProgress, [0, 1], [0, -700]);
+    const imageOpacity = useTransform(scrollYProgress, [0, 0.3], [0, 1]);
 
 
     return (
         <motion.div
-            style={{ y: imageY }}
-            initial={{ opacity: 0}}
-            whileInView={{ opacity: 1 }}
-            transition={{ duration: 1 }}
-            viewport={{ once: false }}
+            style={{ y: imageY, opacity: imageOpacity }}          
 
             className="bg-cyber-goddess md:bg-cover h-full md:h-[1526px] absolute w-full z-0 bg-contain bg-bottom md:bg-top bg-no-repeat"
         >
